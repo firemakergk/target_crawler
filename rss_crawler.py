e0db4b78a0995efe4fe70d73b7ec515d510c69c5
finetu <glf-19911222>
2015-05-28 14:56:23 +0800

    rss 加入cache

--- rss_crawler.py
+++ rss_crawler.py
@@ -13,18 +13,19 @@ import feedparser
 class RssCrawler:
 
     def transJson(self,rss):
-        str = '{'
-        str += '\"title\":\"'+rss.feed.title.replace('\"','\\\"')+'\",\"link\":\"'+rss.feed.link.replace('\"','\\\"')+'\",\"subtitle\":\"\",'
-        str += '\"items\":['
+        # str = '{'
+        # str += '\"title\":\"'+rss.feed.title.encode('utf-8').replace('\"','\\\"')+'\",\"link\":\"'+rss.feed.link.encode('utf-8').replace('\"','\\\"')+'\",\"subtitle\":\"\",'
+        str = '['
+        print rss.feed.title.encode('utf-8').replace('\"','\\\"')
         for i in rss.entries:
-            str += '{\"text\":\"'+i['title'].replace('\"','\\\"')+'\",\"href\":\"'+i['link'].replace('\"','\\\"')+'\"},'
+            str += '{\"text\":\"'+i['title'].encode('utf-8').replace('\"','\\\"')+'\",\"href\":\"'+i['link'].encode('utf-8').replace('\"','\\\"')+'\"},'
         str = str[0:-1]
-        str += ']}'
+        str += ']'
         return str
 
     def crawlRss(self):
         try:
-            self.conn = MySQLdb.connect(host="localhost", user="webmoudel", passwd="newsMetro01", db="newsmetro", port=3306, charset="utf8")
+            self.conn = MySQLdb.connect(host="localhost", user="root", passwd="root", db="newsmetro", port=3306, charset="utf8")
         except MySQLdb.Error,e:
             sys.stderr.write("Mysql Error %d: %s" % (e.args[0], e.args[1]))
         cur = self.conn.cursor()
@@ -32,17 +33,19 @@ class RssCrawler:
         self.conn.commit()
         for t in cur:
             #yield {'id': t[0], 'url': t[3], 'xpath': t[5], 'regex': t[6],'md5': t[7], 'status': t[9]}
+            print '--------crawling '+t[3] + '---------'
             rss = feedparser.parse(t[3])
             rssJson = self.transJson(rss)
-            dataVal = (t[0], rssJson , time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+            jsonMd5 = hashlib.md5(rssJson).hexdigest()
+            dataVal = (t[0], rssJson , jsonMd5, time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
             cur1 = self.conn.cursor()
-            cur1.execute('select count(*) from target_mapping where target_id = %s',t[0])
+            cur1.execute('select count(*) from target_mapping where target_id = %s',(t[0],))
             count = cur1.fetchone()
             cur1.close()
-            if count > 0:
-                self.conn.cursor().execute('update target_mapping set items=%s,update_time=%s where target_id=%s', (rssJson,time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),t[0]))
+            if count[0] > 0:
+                self.conn.cursor().execute('update target_mapping set items=%s,md5=%s,update_time=%s where target_id=%s', (rssJson,jsonMd5,time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),t[0]))
             else:
-                self.conn.cursor().execute('insert into target_mapping(target_id,items,update_time) values(%s,%s,%s)', dataVal)
+                self.conn.cursor().execute('insert into target_mapping(target_id,items,md5,update_time) values(%s,%s,%s,%s)', dataVal)
         cur.close()
 
 
